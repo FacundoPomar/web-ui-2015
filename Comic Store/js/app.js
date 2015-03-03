ee61ddd9a16b9fe2c2658d6ca3066e5a55a47a4d
FacundoPomar <facundopomar@hotmail.com>
2015-03-03 17:26:41 -0300

    General Update

--- Comic Store/js/app.js
+++ Comic Store/js/app.js
@@ -1,15 +1,24 @@
 var app = app || {};
 
+(function () {
+var session = JSON.parse(localStorage.getItem("session"));
+if (session) {
+	app.session = session;
+}
+})();
+
 app.events = _.extend({}, Backbone.Events);
 
 
-new app.LoginView({model: new app.Button({text: "login", type: "btn-info"})});
+var vi = new app.LoginView({model: new app.Button({text: "login", type: "btn-info"})});
 
-app.Users.fetch();
 
-app.Comics.fetch();
+$("#login-enter").click (function () {
+	app.events.trigger("login:loginenter");
+});
 
 
 var a = new app.Comic();
 a.set("name", "El ataque de los simios");
 new app.ComicView({model: a});
+
