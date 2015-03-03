2939f69e5f4729d7c6f22c6723944baa60ede577
FacundoPomar <facundopomar@hotmail.com>
2015-03-03 17:25:22 -0300

    update login view

--- Comic Store/js/views/views.js
+++ Comic Store/js/views/views.js
@@ -23,7 +23,7 @@ app.LoginView = Backbone.View.extend({
 	el: "#btn-login",
 	container: "#page-title",
 
-	template: Handlebars.compile('<div class="btn {{ size }} {{ type }}" id="btn-login" data-toggle="modal" data-target="#loginModal">{{ text }}</div>'),
+	loginTemplate: Handlebars.compile('<div class="btn {{ size }} {{ type }}" id="btn-login" data-toggle="modal" data-target="#loginModal">{{ text }}</div>'),
 
 	initialize: function () {
 		//console.log($(this.container));
@@ -31,18 +31,21 @@ app.LoginView = Backbone.View.extend({
 		this.render();
 		this.listenTo(app.events, "login:logout", this.logout);
 		this.listenTo(app.events, "login:login", this.login);
+		this.listenTo(app.events, "login:loginenter", this.checkLogin);
 
-	},
-
-	events: {
-		"click #btn-login": "openModal"
 	},
 
 	render: function () {
-		$(this.container).append( this.template( this.model.attributes))
+		if (app.session) {
+			this.showName();
+			$(this.container).append('<div class="btn btn-sm btn-alert" id="btn-logout">Logout</div>');
+		} else {
+			$(this.container).append( this.loginTemplate( this.model.attributes));
+		}
 	},
 
 	logout: function () {
+		$("#user-name").fadeOut().remove();
 		$(this.$el.selector).fadeIn();
 	},
 
@@ -50,8 +53,55 @@ app.LoginView = Backbone.View.extend({
 		$(this.$el.selector).fadeOut();
 	},
 
-	openModal: function () {
-		console.log("open modal");
+	checkLogin: function () {
+		var user = $("#username").val();
+		var pass = $("#pass").val();
+		if (user) {
+			if (pass) {
+				var userr = app.Users.find(function(model) { return model.get('username') == user; });
+				if (userr && (userr.get("pass") === pass)) {
+					app.session = {id:userr.get("id"), name:userr.get("name")};
+					localStorage.setItem("session", JSON.stringify(app.session));
+					this.loginMsg("Login Like: " + user, "success", true, [$("username"), $("pass")]);
+					this.login();
+					this.showName();
+				} else {
+					this.loginMsg("UserName or Password incorrect", "danger");
+				}
+			} else {
+				this.loginMsg("Pass Empty", "danger");
+			}
+		} else {
+			this.loginMsg("User Empty", "danger");
+		}
+	},
+
+	loginMsg: function (msg, type, correct, inputs) {
+		if ($("#alert-login").length) {
+			$("#alert-login").remove();
+		}
+			$("<div></div>")
+			.addClass("alert alert-" + type)
+			.html(msg)
+			.attr("id", "alert-login")
+			.hide()
+			.prependTo(
+				$("#login-modal-body"))
+			.slideDown();
+		
+		if (correct) {
+			setTimeout(function () { 
+				_.map(inputs, function (elem) {
+					elem.val("");
+				});
+				$("#loginModal").modal("hide");
+				
+			}, 1000);
+		}
+	},
+
+	showName: function () {
+		$("<small></small>").attr("id", "user-name").html("Hello " + app.session.name).hide().appendTo($(this.container)).fadeIn();
 	}
 
 
