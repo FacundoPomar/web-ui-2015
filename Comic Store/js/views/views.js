99253f9462b57c4d84c562cb071162025c7ad4bf
FacundoPomar <facundopomar@hotmail.com>
2015-03-04 10:40:07 -0300

    Refactoring LoginView :)

--- Comic Store/js/views/views.js
+++ Comic Store/js/views/views.js
@@ -18,12 +18,13 @@ app.ComicView = Backbone.View.extend({
 
 });
 
-app.LoginView = Backbone.View.extend({
+/*app.LoginView = Backbone.View.extend({
 
 	el: "#btn-login",
 	container: "#page-title",
 
-	loginTemplate: Handlebars.compile('<div class="btn {{ size }} {{ type }}" id="btn-login" data-toggle="modal" data-target="#loginModal">{{ text }}</div>'),
+	logoutBtnTemplate: Handlebars.compile('<div class="btn {{ size }} {{ type }}" id="btn-logout">Logout</div>'),
+	loginBtnTemplate: Handlebars.compile('<div class="btn {{ size }} {{ type }}" id="btn-login" data-toggle="modal" data-target="#loginModal">{{ text }}</div>'),
 
 	initialize: function () {
 		//console.log($(this.container));
@@ -47,7 +48,7 @@ app.LoginView = Backbone.View.extend({
 	logout: function () {
 		console.log("trigger");
 		app.session = undefined;
-		localStorage.removeItem("session");;
+		localStorage.removeItem("session");
 		$("#btn-logout").fadeOut().delay(100).remove();
 		$("#user-name").fadeOut().delay(100).remove();
 		this.showLogin();
@@ -85,14 +86,14 @@ app.LoginView = Backbone.View.extend({
 		if ($("#alert-login").length) {
 			$("#alert-login").remove();
 		}
-			$("<div></div>")
-			.addClass("alert alert-" + type)
-			.html(msg)
-			.attr("id", "alert-login")
-			.hide()
-			.prependTo(
-				$("#login-modal-body"))
-			.slideDown();
+		$("<div></div>")
+		.addClass("alert alert-" + type)
+		.html(msg)
+		.attr("id", "alert-login")
+		.hide()
+		.prependTo(
+			$("#login-modal-body"))
+		.slideDown();
 		
 		if (correct) {
 			setTimeout(function () { 
@@ -118,7 +119,106 @@ app.LoginView = Backbone.View.extend({
 	}
 
 
+});*/
+
+
+//Futuro Login View.
+app.LoginView = Backbone.View.extend({
+
+	el: $("#login-box"),
+	//Use external templates ?
+	logoutBtnTemplate: Handlebars.compile('<div class="btn {{ size }} {{ type }}" id="btn-logout">Logout</div>'),
+	
+	loginBtnTemplate: Handlebars.compile('<div class="btn {{ size }} {{ type }}" id="btn-login" data-toggle="modal" data-target="#loginModal">{{ text }}</div>'),
+	
+	nameTemplate: Handlebars.compile('<small id="user-name">Hello {{ name }} !</small>'),
+
+	events: {
+		"click #btn-logout": "logout",
+		"click #login-enter": "login"
+	},
+
+	initialize: function () {
+
+		this.$el.append( $(this.logoutBtnTemplate( this.model.attributes)).hide());
+		this.$el.append( $(this.loginBtnTemplate( this.model.attributes)).hide());
+		this.$el.append( $("#modal-template").html());
+		
+		if (app.session) {
+			$("#btn-logout").fadeIn();
+			this.$el.append( $(this.nameTemplate( {name: app.session.name})));
+		} else {
+			this.$el.append( $(this.nameTemplate( {name: ""})).hide());
+			$("#btn-login").fadeIn();
+		}
+	},
+
+	logout: function () {
+		if (app.session) {
+			app.session = undefined;
+			localStorage.removeItem("session");
+			$("#btn-logout").hide();
+			$("#btn-login").fadeIn();
+			$("#user-name").fadeOut();
+		}
+	},
+
+	login: function () {
+		if (!app.session) {
+			var user = $("#username").val();
+			var pass = $("#pass").val();
+			if (user) {
+				if (pass) {
+					var userr = app.Users.find(function(model) { return model.get('username') == user; });
+					if (userr && (userr.get("pass") === pass)) {
+						app.session = {id:userr.get("id"), name:userr.get("name")};
+						localStorage.setItem("session", JSON.stringify(app.session));
+						
+						$("#btn-login").fadeOut();
+						$("#btn-logout").fadeIn();
+						$("#user-name").html( this.nameTemplate({name: app.session.name})).fadeIn();
+						$("#loginModal").modal("hide");
+					} else {
+						this.loginMsg("UserName or Password incorrect", "danger");
+					}
+				} else {
+					this.loginMsg("Pass Empty", "danger");
+				}
+			} else {
+				this.loginMsg("User Empty", "danger");
+			}
+		}
+		this.emptyForm();
+	},
+
+	loginMsg: function (msg, type, correct, inputs) {
+		if ($("#alert-login").length) {
+			$("#alert-login").remove();
+		}
+		$("<div></div>")
+		.addClass("alert alert-" + type)
+		.html(msg)
+		.attr("id", "alert-login")
+		.hide()
+		.prependTo(
+			$("#login-modal-body"))
+		.slideDown();
+		
+		if (correct) {
+			setTimeout(function () { 
+				_.map(inputs, function (elem) {
+					elem.val("");
+				});
+				$("#loginModal").modal("hide");
+				
+			}, 1000);
+		}
+	},
+
+	emptyForm: function () {
+		$("#alert-login").remove();
+		$("#username").val("");
+		$("#pass").val("")
+	}
 });
-
-
 
