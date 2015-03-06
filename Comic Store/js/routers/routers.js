53a84256e27f053956612d43c1df1ff79ccafe2d
FacundoPomar <facundopomar@hotmail.com>
2015-03-06 17:47:01 -0300

    added routing functions to sections genres, editions, characters and profile

--- Comic Store/js/routers/routers.js
+++ Comic Store/js/routers/routers.js
@@ -4,14 +4,17 @@ app.Router = Backbone.Router.extend({
 	
 	routes: {
 		"": "home",
-		"(/)": "home",
+		"/": "home",
 		"home(/)": "home",
 		"news(/)": "news",
-		"games(/)": "games"
+		"genres(/)": "genres",
+		"editions(/)": "editions",
+		"characters(/)": "characters",
+		"user/:query": "profile"
 	},
 
 	home: function () {
-		this.navigate("/home/", {pushState: true});
+		this.navigate("./", {pushState: true});
 		app.homeView.render();
 	},
 
@@ -20,8 +23,24 @@ app.Router = Backbone.Router.extend({
 		app.newsView.render();
 	},
 
-	games: function () {
-		this.navigate("/games/", {pushState: true});
-		app.gamesView.render();
+	genres: function () {
+		this.navigate("/genres/", {pushState: true});
+		app.genresView.render();
+	},
+
+	editions: function () {
+		this.navigate("/editions/", {pushState: true});
+		app.editionsView.render();
+	},
+
+	characters: function () {
+		this.navigate("/characters/", {pushState: true});
+		app.charactersView.render();
+	},
+
+	profile: function (query) {
+		this.navigate("/user/" + query, {pushState: true});
+		app.userView = new app.UserProfileView();
+		app.userView.render(query);
 	}
 });
