136f906fa5b68eaf05d4a5f683c54204226fb596
FacundoPomar <facundopomar@hotmail.com>
2015-03-06 15:21:03 -0300

    router create

---
+++ Comic Store/js/routers/routers.js
@@ -0,0 +1,26 @@
+var app = app || {};
+
+app.Router = Backbone.Router.extend({
+	
+	routes: {
+		"(/)": "home",
+		"home(/)": "home",
+		"news(/)": "news",
+		"games(/)": "games"
+	},
+
+	home: function () {
+		this.navigate("home/", {pushState: true});
+		app.homeView.render();
+	},
+
+	news: function () {
+		this.navigate("news/", {pushState: true});
+		app.newsView.render();
+	},
+
+	games: function () {
+		this.navigate("games/", {pushState: true});
+		app.gamesView.render();
+	}
+});
